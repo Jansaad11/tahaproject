140ceee20a67955e582d07828cb28d200d9dcfbf
Jansaad <saadaptech11@gmail.com>
2025-05-31 11:46:31 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' 
 export default defineConfig({ 
   plugins: [react()] ,
-  base: '/react-app', // <-- Use your repo name here 
+  base: '/tahaproject', // <-- Use your repo name here 
 }) 
